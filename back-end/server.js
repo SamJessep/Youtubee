8ce39f7bcb95135ae6ff8b0be32c5b5876941f6b
sam <samjessep77@gmail.com>
2021-02-02 18:29:13 +1300

    server update

--- back-end/server.js
+++ back-end/server.js
@@ -100,7 +100,7 @@ app.get('/download',async (req, res) => {
   let outFileType
   let outFileName
   if(combinedFile){
-    const fileName = __dirname+"\\"+combinedFile;
+    const fileName = __dirname+"/"+combinedFile;
     var extension = path.extname(fileName);
     var name = path.basename(fileName,extension);
     console.log("COMBINED", fileName, extension, name)
