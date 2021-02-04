f5ed95162d90f9bcd1c56dd53ce53a04280f68a2
sam <samjessep77@gmail.com>
2021-02-04 23:39:15 +1300

    added shell for notifications

--- front-end/public/main.js
+++ front-end/public/main.js
@@ -1,9 +1,14 @@
 // Register the service worker if available.
-if ('serviceWorker' in navigator) {
+const applicationServerPublicKey = "BOYuHV4mnhvdYjV_q8GOXI9pY8t-J0Z_2kdS6pFhPGAXwXtQPOUwZnSL513P-_Xv75FJMEDIx-lJ2kwMisTOQ1o"
+var swRegistration;
+
+if ('serviceWorker' in navigator && 'PushManager' in window) {
     navigator.serviceWorker.register('./sw.js').then(function(reg) {
-        console.log('Successfully registered service worker', reg);
+      swRegistration = reg
+      initializeUI()
+      console.log('Successfully registered service worker', reg);
     }).catch(function(err) {
-        console.warn('Error whilst registering service worker', err);
+      console.warn('Error whilst registering service worker', err);
     });
 }
 
@@ -20,12 +25,52 @@ window.addEventListener('offline', function(e) {
 
 }, false);
 
-// Check if the user is connected.
-if (navigator.onLine) {
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
 
-} else {
-    // Show offline message
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
-// Set Knockout view model bindings.
+function initializeUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      subscribeUser();
+      console.log('User is NOT subscribed.');
+    }
+  });
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+    updateSubscriptionOnServer(subscription);
+    isSubscribed = true;
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+  });
+}
+
+function updateSubscriptionOnServer(subscription){
+  console.log("SERVER", subscription)
+}
--- front-end/public/sw.js
+++ front-end/public/sw.js
@@ -26,3 +26,49 @@ self.addEventListener('fetch', function(event) {
    })
  );
 });
+
+
+self.addEventListener('push', function(event) {
+  //Remove website name from notification
+  const title = 'Youtubee';
+  const options = {
+    body: 'Your download is ready',
+    icon: '/icons/download-notification.png',
+    badge: '/icons/download-notification.png',
+    //image: video thumbnail,
+    //Change this to video specific tag later
+    tag: 'download',
+    renotify: true,
+    requireInteraction: true,
+    actions: [
+      { "action": "download", "title": "Download" },
+      { "action": "cancel", "title": "Cancel" }
+    ]
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.', event);
+  event.notification.close();
+
+  if ('actions' in Notification.prototype) {
+  // Action buttons are supported.
+    if(event.action == "download"){
+      //Download file directly
+      // event.data.json()
+      // event.data.text()
+    }else if (event.action == "cancel"){
+      //Delete file from server
+    }else {
+      //Show download screen gui
+    }
+  } else {
+    // Action buttons are NOT supported.
+    // download file OR Show download screen gui
+  }
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
